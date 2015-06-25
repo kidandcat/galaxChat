b3e0ea59e232e9ec3893abeec191c410a8fa0aa3
Asky <kidandcat@gmail.com>
2015-06-25 16:39:35 +0200

    autoCommit

--- routes/socket.js
+++ routes/socket.js
@@ -23,8 +23,8 @@ module.exports.listen = function (app, console){
 	var votePeople = 0;
 	var LineByLineReader = require('line-by-line');
 	//listen express app
-	ioMain = socketio.listen(app)
-	io = ioMain.of('/chat');
+	var ioMain = socketio.listen(app)
+	var io = ioMain.of('/chat');
 	//set log mode to normal, if not it is default set in debug mode
 	//io.set('log level', 1);
 	function time() {
