271ea7345967485b2801c1d0bfce37006cd40014
Asky <kidandcat@gmail.com>
2015-06-25 17:37:32 +0200

    autoCommit

--- app.js
+++ app.js
@@ -71,35 +71,7 @@ app.use(session({
     }
 }));
 var galaxIO = socketio.listen(secureserver);
-galaxIO.use(function(socket, next) {
-    try {
-        var data = socket.handshake || socket.request;
-        if (! data.headers.cookie) {
-            return next(new Error('Missing cookie headers'));
-        }
-        console.log('cookie header ( %s )', JSON.stringify(data.headers.cookie));
-        var cookies = cookie.parse(data.headers.cookie);
-        console.log('cookies parsed ( %s )', JSON.stringify(cookies));
-        if (! cookies[COOKIE_NAME]) {
-            return next(new Error('Missing cookie ' + COOKIE_NAME));
-        }
-        /*ar sid = cookieParser.signedCookie(cookies[COOKIE_NAME], COOKIE_SECRET);
-        if (! sid) {
-            return next(new Error('Cookie signature is not valid'));
-        }*/
-        console.log('session ID ( %s )', sid);
-        data.sid = sid;
-        sessionStore.get(sid, function(err, session) {
-            if (err) return next(err);
-            if (! session) return next(new Error('session not found'));
-            data.session = session;
-            next();
-        });
-    } catch (err) {
-        console.error(err.stack);
-        next(new Error('Internal server error'));
-    }
-});
+
 
 app.use('/files', serveIndex('public/uploaded/files', {'icons': true}))
 app.use('/files', express.static(path.join(__dirname, 'public/uploaded/files')));
