e1bebce3dc3d918f1cff05a8699294d8bf7d7b71
Asky <kidandcat@gmail.com>
2015-06-25 16:20:48 +0200

    autoCommit

--- app.js
+++ app.js
@@ -90,6 +90,7 @@ app.use(function(err, req, res, next) {
 var secureserver = httpsserver.listen(80);
 //var io = require('./routes/socket').listen(server, console);
 var io = require('./routes/socket').listen(secureserver, console);
+var io2 = require('./routes/socket').listen(app.listen(443), console, cookieParser, session);
 module.exports = app;
 console.log('           SERVER  RUNNING'.magenta);
 console.log('          developed by Asky'.rainbow);
